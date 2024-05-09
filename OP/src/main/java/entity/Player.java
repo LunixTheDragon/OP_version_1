149c7325ad2d9d13a835a97ef9d8bf388e684266
Kelmet <tom@lelek.eu>
2024-05-09 07:51:44 +0200

    Little hints added in TODOs

--- OP/src/main/java/entity/Player.java
+++ OP/src/main/java/entity/Player.java
@@ -21,10 +21,19 @@ public class Player extends Entity {
     }
     public void loadAni(){
         BufferedImage[][] img = LoadSave.GetSpriteAtlas("player_sprites.png");
-        int spriteWidth = 32; // Width of each sprite
+        int spriteWidth = width; // Width of each sprite
         int spriteHeight = 32; // Height of each sprite
 
         // Example: Extract a sub-image for the first animation frame
+        /* TODO: I have no fucking idea what this is and it should be something like this:
+            for (int j = 0; j < animations.length; j++)
+                for (int i = 0; i < animations[j].length; i++)
+                    animations[j][i] = img.getSubimage(i * {playerWidth}, j * {playerHeight}, {playerWidth}, {playerHeight})
+                                                            ^^^ 64 ^^^          ^^^ 40 ^^^     ^^^ 64 ^^^      ^^^ 40 ^^^
+            this will ensure to load all animations
+            ___________________
+            Note: you have to handle each animation because they have different sizes
+         */
         animations = new BufferedImage[1][5];
         for (int i = 0; i < animations[0].length; i++) {
             animations[0][i] = img[0][i].getSubimage(i * spriteWidth, 0, spriteWidth, spriteHeight);
